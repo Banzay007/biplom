b831d28d36bca485acef5306edaa684bf74e98c6
Banzay007 <olga_sergeeva75@mail.ru>
2021-05-29 16:46:50 +0300

    финал

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
-// автоматическое перемезение к началу страницы
-var smoothJumpUp = function () {
-	if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-		window.scrollBy(0, -50);
-		setTimeout(smoothJumpUp, 10);
-	}
-}
+$(function () {
 
-// показ и скрытие прокрутки от положения
-window.onscroll = function () {
-	var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-	if (scrolled > 100) {
-		document.getElementById('upbutton').style.display = 'block';
-	} else {
-		document.getElementById('upbutton').style.display = 'none';
+	// автоматическое перемещение к началу страницы
+	var smoothJumpUp = function () {
+		if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+			window.scrollBy(0, -50);
+			setTimeout(smoothJumpUp, 10);
+		}
 	}
-}
+
+	// показ и скрытие прокрутки от положения
+	window.onscroll = function () {
+		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+		if (scrolled > 100) {
+			document.getElementById('upbutton').style.display = 'block';
+		} else {
+			document.getElementById('upbutton').style.display = 'none';
+		}
+	}
+
+
+
+
+
+	// Smooth scroll
+	$("[data-scroll]").on("click", function (event) {
+		event.preventDefault();
+
+		var $this = $(this),
+			blockId = $this.data('scroll'),
+			blockOffset = $(blockId).offset().top;
+
+		$("#nav a").removeClass("active");
+		$this.addClass("active");
+
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 500);
+	});
+
+
+
+	// Menu nav_toggle
+
+	$("#nav_toggle").on("click", function (event) {
+		event.preventDefault();
+
+		$(this).toggleClass("active");
+		$("#nav").toggleClass("active");
+	});
+
+
+
+
+});
